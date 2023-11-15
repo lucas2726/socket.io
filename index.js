f325127e3940a53eee957fb43cede0d9405d345a
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-11-15 19:34:53 -0400

    ss

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-let express = require("express")
-let app = express()
-let http = require("http").createServer(app) /*Para dizer que vamos usar http no lugar de app paraa criar o servidor*/
-let io = require("socket.io")(http)
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
+io.on("connection", (socket) => {
 
-io.on("connection",(socket) => {
+    socket.on("disconnect", (socket) => {
+        console.log("X desconectou: " + socket.id)
+    })
+
     socket.on("boasvindas", (data) => {
         console.log("Executando evento de boas vindas")
         console.log(data)
+    })
+    socket.on("palavra", (data) => {
+        console.log(data)
+        socket.emit("resultado", data + " - Guia do programador!")
     })
 })
 
@@ -17,6 +28,6 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-http.listen(3000, () => {
+Server.listen(3000, () => {
     console.log("APP rodando!")
 })
