9bd0caf4f9436b006490ea664babaa34dde34412
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-11-16 10:47:33 -0400

    ee

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+
 
 io.on("connection", (socket) => {
 
@@ -28,6 +30,6 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-Server.listen(3000, () => {
+server.listen(3000, () => {
     console.log("APP rodando!")
 })
